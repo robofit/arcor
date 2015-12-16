5d344e929a5be36e9e37285adddd8e948eece4f2
Michal Kapinus <ikapinus@fit.vutbr.cz>
2015-12-16 13:08:30 +0100

    Added fake sender for testing purposes, working MSG to JSON conversion

--- art_tablet_bridge/scripts/bridge_to_file.py
+++ art_tablet_bridge/scripts/bridge_to_file.py
@@ -3,37 +3,32 @@ import rospy
 import jsonpickle
 from std_msgs.msg import Int64
 from geometry_msgs.msg import Pose, Point, Quaternion
+from object_recognizer_msgs.msg import InstancesArray, ObjInstance
 
 
 class BridgeToFile:
 
     def __init__(self):
-        rospy.Subscriber("/art_object_detector/object", Int64, self.callback)
+        rospy.Subscriber("/art_object_detector/object", InstancesArray, self.callback)
         rospy.spin()
 
     def callback(self, detected_object):
         """
-        :type detected_object: Int64
+        :type detected_object: InstancesArray
         :return:
         """
-        rospy.loginfo(rospy.get_caller_id() + ": I Heard " + str(detected_object.data))
-        detected = {'header': {'timestamp': 0, 'frame_id': 'dont_care', 'seq': 0}, 'objects': [
-            {'object_id': 'obj1', 'pose': Pose(Point(1, 2, 3), Quaternion(0, 0, 0, 1))},
-            {'object_id': 'obj2', 'pose': Pose(Point(4, 25, 6), Quaternion(1, 2, 0, 1))},
-            {'object_id': 'obj3', 'pose': Pose(Point(8, 2, 4), Quaternion(0, 8, 0, 1))}
-        ]}
 
         objects = []
-        for obj in detected['objects']:
-            to_json = {'object_id': obj['object_id'], 'pose': {'position': {'x': obj['pose'].position.x,
-                                                                            'y': obj['pose'].position.y,
-                                                                            'z': obj['pose'].position.z},
-                                                               'orientation': {'x': obj['pose'].orientation.x,
-                                                                               'y': obj['pose'].orientation.y,
-                                                                               'z': obj['pose'].orientation.z,
-                                                                               'w': obj['pose'].orientation.w}}}
+        for obj in detected_object.instances:
+            to_json = {'object_id': obj.object_id, 'pose': {'position': {'x': obj.pose.position.x,
+                                                                         'y': obj.pose.position.y,
+                                                                         'z': obj.pose.position.z},
+                                                            'orientation': {'x': obj.pose.orientation.x,
+                                                                            'y': obj.pose.orientation.y,
+                                                                            'z': obj.pose.orientation.z,
+                                                                            'w': obj.pose.orientation.w}}}
             objects.append(to_json)
-        # rospy.loginfo(jsonpickle.encode({'objects': objects}))
+        rospy.loginfo(jsonpickle.encode({'objects': objects}))
         with open('/home/ikapinus/objects.json', 'w') as f:
             f.write(jsonpickle.encode({'objects': objects}))
 
