2e2ba3e5cb8be1a6cf498fc57cd38c2eab779b94
Kapim <ikapinus@fit.vutbr.cz>
2016-03-09 12:45:29 +0100

    simple arcode objects detector

---
+++ art_detectors/art_arcode_detector/src/detector.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+from ar_track_alvar_msgs.msg import AlvarMarkers, AlvarMarker
+from art_object_recognizer_msgs.msg import ObjInstance, InstancesArray
+import sys
+import rospy
+import dataset
+
+
+class ArCodeDetector:
+    objects_db = None
+    objects_table = None
+
+    def __init__(self):
+        self.objects_db = dataset.connect('sqlite:///objdatabase.db')
+        self.objects_table = self.objects_db['objects']
+        # self.objects_table.insert(dict(name="box", model_url="blablabla", obj_id=14))
+        # self.objects_table.insert(dict(name="cup", model_url="blablabla", obj_id=9))
+        # self.objects_table.update(dict(name="juice", model_url="blablabla", obj_id=0), ['name'])
+        self.ar_code_sub = rospy.Subscriber("ar_pose_marker", AlvarMarkers, self.ar_code_cb)
+        self.detected_objects_pub = rospy.Publisher("/art_object_detector/object", InstancesArray, queue_size=10)
+        for ar in self.objects_table:
+            print ar
+        while not rospy.is_shutdown():
+            pass
+
+    def ar_code_cb(self, data):
+        rospy.logdebug("New arcodes arrived:")
+
+        for arcode in data.markers:
+            obj = self.objects_table.find_one(obj_id=int(arcode.id))
+            # rospy.logdebug(object if object is not None else "Object not in database")
+            if obj is not None:
+                rospy.logdebug(obj)
+                instances = InstancesArray()
+                obj_in = ObjInstance()
+                obj_in.object_id = str(obj['obj_id'])
+                obj_in.pose = arcode.pose.pose
+                instances.instances.append(obj_in)
+                self.detected_objects_pub.publish(instances)
+            else:
+                rospy.logdebug("Object not in database")
+        if len(data.markers) == 0:
+            rospy.logdebug("Empty")
+
+
+if __name__ == '__main__':
+    rospy.init_node('art_arcode_detector')
+    # rospy.init_node('art_arcode_detector', log_level=rospy.DEBUG)
+    try:
+        node = ArCodeDetector()
+    except rospy.ROSInterruptException:
+        pass
