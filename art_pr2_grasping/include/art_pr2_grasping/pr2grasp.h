2318a50f404a47e89a8b30fcae74f9b00ab03ee5
Materna Zdenek <imaterna@fit.vutbr.cz>
2016-03-16 15:56:25 +0100

    art_pr2_grasping: alpha

--- art_pr2_grasping/include/art_pr2_grasping/pr2grasp.h
+++ art_pr2_grasping/include/art_pr2_grasping/pr2grasp.h
@@ -20,39 +20,158 @@ namespace art_pr2_grasping {
       moveit_simple_grasps::GraspData grasp_data_;
 
       // our interface with MoveIt
-      boost::scoped_ptr<move_group_interface::MoveGroup> move_group_;
-
-      // which baxter arm are we using
-      std::string arm_;
-      std::string planning_group_name_;
+      // todo vector/map of groups?
+      boost::scoped_ptr<move_group_interface::MoveGroup> move_group_l_;
+      boost::scoped_ptr<move_group_interface::MoveGroup> move_group_r_;
       
       ros::NodeHandle nh_;
       
     public:
     
       artPr2Grasping()
-        : arm_("left"),
-          planning_group_name_(arm_ + "_arm"),
-          nh_("~") {
+        : nh_("~") {
           
           // Create MoveGroup for one of the planning groups
-          move_group_.reset(new move_group_interface::MoveGroup(planning_group_name_));
-          move_group_->setPlanningTime(30.0);
+          move_group_l_.reset(new move_group_interface::MoveGroup("left_arm"));
+          move_group_l_->setPlanningTime(30.0);
+          move_group_l_->allowLooking(false); // true causes failure
+          move_group_l_->allowReplanning(true);
+          move_group_l_->setGoalTolerance(0.005);
+          move_group_l_->setPlannerId("RRTConnectkConfigDefault");
+
+          move_group_r_.reset(new move_group_interface::MoveGroup("right_arm"));
+          move_group_r_->setPlanningTime(30.0);
+          move_group_r_->allowLooking(false); // true causes failure
+          move_group_r_->allowReplanning(true);
+          move_group_r_->setGoalTolerance(0.005);
+          move_group_r_->setPlannerId("RRTConnectkConfigDefault");
 
           // Load grasp generator
-          if (!grasp_data_.loadRobotGraspData(nh_, arm_))
+          if (!grasp_data_.loadRobotGraspData(nh_, "left"))
             ros::shutdown();
 
           // Load the Robot Viz Tools for publishing to rviz
-          visual_tools_.reset(new moveit_visual_tools::VisualTools( grasp_data_.base_link_));
+          visual_tools_.reset(new moveit_visual_tools::VisualTools( grasp_data_.base_link_, "markers"));
           visual_tools_->setFloorToBaseHeight(-0.9);
-          visual_tools_->loadEEMarker(grasp_data_.ee_group_, planning_group_name_);
+          visual_tools_->loadEEMarker(grasp_data_.ee_group_, "left_arm");
 
           simple_grasps_.reset(new moveit_simple_grasps::SimpleGrasps(visual_tools_));
-          
+
           }
-          
-      
+
+      bool getReady() {
+
+          //move_group_->setJointTarget(nejaka_vychozi_pozice)
+          move_group_l_->setPoseTarget(move_group_l_->getRandomPose());
+          move_group_l_->move();
+          move_group_r_->setPoseTarget(move_group_r_->getRandomPose());
+          move_group_r_->move();
+          return true;
+
+      }
+
+      // todo remove id - the robot should know what's in his hand
+      bool place(const std::string &id, const std::string &group, const geometry_msgs::PoseStamped &ps) {
+
+          std::vector<moveit_msgs::PlaceLocation> place_locations;
+          std::vector<moveit_msgs::Grasp> grasps;
+
+          // todo transform ps into grasp_data_.base_link_
+
+          // Re-usable datastruct
+          geometry_msgs::PoseStamped pose_stamped;
+          pose_stamped.header.frame_id = grasp_data_.base_link_;
+          pose_stamped.header.stamp = ros::Time::now();
+
+
+          // Create 360 degrees of place location rotated around a center
+          for (double angle = 0; angle < 2*M_PI; angle += M_PI/2)
+          {
+            pose_stamped.pose = ps.pose;
+
+            // Orientation
+            Eigen::Quaterniond quat(Eigen::AngleAxis<double>(double(angle), Eigen::Vector3d::UnitZ()));
+            pose_stamped.pose.orientation.x = quat.x();
+            pose_stamped.pose.orientation.y = quat.y();
+            pose_stamped.pose.orientation.z = quat.z();
+            pose_stamped.pose.orientation.w = quat.w();
+
+            // Create new place location
+            moveit_msgs::PlaceLocation place_loc;
+
+            place_loc.place_pose = pose_stamped;
+
+            visual_tools_->publishBlock( place_loc.place_pose.pose, moveit_visual_tools::BLUE, 0.05);
+
+            // Approach
+            moveit_msgs::GripperTranslation pre_place_approach;
+            pre_place_approach.direction.header.stamp = ros::Time::now();
+            pre_place_approach.desired_distance = grasp_data_.approach_retreat_desired_dist_; // The distance the origin of a robot link needs to travel
+            pre_place_approach.min_distance = grasp_data_.approach_retreat_min_dist_; // half of the desired? Untested.
+            pre_place_approach.direction.header.frame_id = grasp_data_.base_link_;
+            pre_place_approach.direction.vector.x = 0;
+            pre_place_approach.direction.vector.y = 0;
+            pre_place_approach.direction.vector.z = -1; // Approach direction (negative z axis)  // TODO: document this assumption
+            place_loc.pre_place_approach = pre_place_approach;
+
+            // Retreat
+            moveit_msgs::GripperTranslation post_place_retreat;
+            post_place_retreat.direction.header.stamp = ros::Time::now();
+            post_place_retreat.desired_distance = grasp_data_.approach_retreat_desired_dist_; // The distance the origin of a robot link needs to travel
+            post_place_retreat.min_distance = grasp_data_.approach_retreat_min_dist_; // half of the desired? Untested.
+            post_place_retreat.direction.header.frame_id = grasp_data_.base_link_;
+            post_place_retreat.direction.vector.x = 0;
+            post_place_retreat.direction.vector.y = 0;
+            post_place_retreat.direction.vector.z = 1; // Retreat direction (pos z axis)
+            place_loc.post_place_retreat = post_place_retreat;
+
+            // Post place posture - use same as pre-grasp posture (the OPEN command)
+            place_loc.post_place_posture = grasp_data_.pre_grasp_posture_;
+
+            place_locations.push_back(place_loc);
+          }
+
+          // Prevent collision with table
+          //move_group_->setSupportSurfaceName(SUPPORT_SURFACE3_NAME);
+
+          return move_group_l_->place(id, place_locations);
+
+      }
+
+      bool pick(const std::string &id, const std::string &group, const geometry_msgs::PoseStamped &ps/*, const shape_msgs::SolidPrimitive & shape*/) {
+
+          visual_tools_->setMuted(false);
+          visual_tools_->cleanupCO(id);
+          visual_tools_->cleanupACO(id);
+
+          visual_tools_->publishBlock(ps.pose, moveit_visual_tools::BLUE, 0.05);
+          visual_tools_->publishCollisionBlock(ps.pose, id, 0.05); // todo ps transformovat do grasp_data_.base_link_
+
+          std::vector<moveit_msgs::Grasp> grasps;
+
+          // todo use generateBoxGrasps ?
+          if (!simple_grasps_->generateBlockGrasps( ps.pose, grasp_data_, grasps )) {
+
+              ROS_ERROR("No grasps found.");
+              return false;
+          }
+
+          /*if (!visual_tools_->publishGrasps(grasps, grasp_data_.ee_parent_link_)) {
+
+              ROS_WARN("Grasp animation failed");
+          }*/
+
+          std::vector<std::string> allowed_touch_objects;
+          allowed_touch_objects.push_back("id");
+
+          // Add this list to all grasps
+          for (std::size_t i = 0; i < grasps.size(); ++i)
+          {
+            grasps[i].allowed_touch_objects = allowed_touch_objects;
+          }
+
+          return move_group_l_->pick(id, grasps);
+      }
   
   };
 
--- art_pr2_grasping/src/pr2grasp.cpp
+++ art_pr2_grasping/src/pr2grasp.cpp
@@ -6,7 +6,40 @@ int main(int argc, char **argv)
 {
   ros::init (argc, argv, "pr2_pick_place");
 
-  artPr2Grasping gr();
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
+
+  artPr2Grasping gr;
+
+  ros::Duration(1).sleep();
+
+  geometry_msgs::PoseStamped ps;
+
+  ps.header.frame_id = "base_footprint";
+  ps.pose.position.x = 0.75;
+  ps.pose.position.y = 0.3;
+  ps.pose.position.z = 0.75;
+  ps.pose.orientation.x = 0.0;
+  ps.pose.orientation.y = 0.0;
+  ps.pose.orientation.z = 0.0;
+  ps.pose.orientation.w = 1.0;
+
+  gr.getReady();
+
+  ROS_INFO("ready");
+
+  if (!gr.pick("1", "left_arm", ps)) {
+
+      ROS_ERROR("pick failed");
+      ros::spin();
+  }
+
+  ps.pose.position.y = 0.1;
+
+  if (!gr.place("1", "left_arm", ps)) {
+
+      ROS_ERROR("place failed");
+  }
 
   ros::spin();
 
