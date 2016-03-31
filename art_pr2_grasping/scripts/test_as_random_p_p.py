3d0c13d12c99cfef376a9eb1ffe673f9fb5529aa
Materna Zdenek <imaterna@fit.vutbr.cz>
2016-03-31 12:27:28 +0200

    art_pr2_grasping: fixed/renamed scripts

--- art_pr2_grasping/scripts/test_as_random_p_p.py
+++ art_pr2_grasping/scripts/test_as_random_p_p.py
@@ -9,54 +9,62 @@ import random
 
 def main():
 
-    client = actionlib.SimpleActionClient('/pr2_pick_place/pp', art_msgs.pickplaceAction)
+    client = actionlib.SimpleActionClient('/pr2_pick_place/pp', art_msgs.msg.pickplaceAction)
     client.wait_for_server()
 
-    rospy.loginfo('sending goal')
+    rospy.loginfo('sending goal - pick')
 
     goal = art_msgs.msg.pickplaceGoal()
-    
     goal.id = "my_object"
-    goal.operation = goal.PICK_AND_PLACE
-    goal.z_axis_angle_increment = (2*3.14)/360*180
-    #goal.z_axis_angle_increment = 0
-    goal.keep_orientation = True
-    
-    arms = [goal.LEFT_ARM, goal.RIGHT_ARM]
-    
-    goal.arm = random.choice(arms)
+    goal.operation = goal.PICK
+    goal.arm = goal.LEFT_ARM
     
     goal.bb = SolidPrimitive()
     goal.bb.type = SolidPrimitive.BOX
     goal.bb.dimensions.append(0.2)
     goal.bb.dimensions.append(0.05)
     goal.bb.dimensions.append(0.05)
+    
+    bbz = goal.bb.dimensions[2]/2
 
     goal.pose = PoseStamped()
     goal.pose.header.frame_id = "base_footprint"
     goal.pose.header.stamp = rospy.Time.now()
     goal.pose.pose.position.x = random.uniform(0.4, 0.7)
     goal.pose.pose.position.y = random.uniform(-0.5, 0.5)
-    goal.pose.pose.position.z = 0.74 + goal.bb.dimensions[2]/2 # vyska stolu + pulka kosticky
+    goal.pose.pose.position.z = 0.74 + bbz # vyska stolu + pulka kosticky
     goal.pose.pose.orientation.x = 0.0
     goal.pose.pose.orientation.y = 0.0
     goal.pose.pose.orientation.z = 0.0
     goal.pose.pose.orientation.w = 1.0
+    
+    client.send_goal(goal)
+    client.wait_for_result()
+
+    rospy.loginfo('got result')
+    print client.get_result()
+    print "status: " + client.get_goal_status_text()
+    print "state: " + str(client.get_state())
+    
+    rospy.loginfo('sending goal - place')
+    
+    goal = art_msgs.msg.pickplaceGoal()
+    goal.id = "my_object"
+    goal.operation = goal.PLACE
+    goal.arm = goal.LEFT_ARM
     
     goal.pose2 = PoseStamped()
     goal.pose2.header.frame_id = "base_footprint"
     goal.pose2.header.stamp = rospy.Time.now()
     goal.pose2.pose.position.x = random.uniform(0.4, 0.7)
     goal.pose2.pose.position.y = random.uniform(-0.5, 0.5)
-    goal.pose2.pose.position.z = 0.74 + goal.bb.dimensions[2]/2
+    goal.pose2.pose.position.z = 0.74 + bbz
     goal.pose2.pose.orientation.x = 0.0
     goal.pose2.pose.orientation.y = 0.0
     goal.pose2.pose.orientation.z = 0.0
     goal.pose2.pose.orientation.w = 1.0
 
-    rospy.loginfo('sending goal')
     client.send_goal(goal)
-
     client.wait_for_result()
 
     rospy.loginfo('got result')
