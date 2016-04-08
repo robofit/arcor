e6971261cad1eb96e0b198a39363f01637030814
Kapim <michal.kapinus@gmail.com>
2016-04-08 16:37:49 +0200

    art_brain initial commit, ar-table-common update

--- art_pick_and_place/src/pick_and_place.py
+++ art_pick_and_place/src/pick_and_place.py
@@ -5,6 +5,7 @@ import rospkg
 import sys
 import actionlib
 import random
+import math
 
 from art_msgs.msg import InstancesArray, ObjInstance
 from art_msgs.msg import pickplaceAction, pickplaceGoal
@@ -15,23 +16,24 @@ objects = {}
 # STATES = ['nothing', 'picked']
 NOTHING = 0
 PICKED = 1
-
+object_key = None
+timestamp = None
 
 class PickAndPlace:
     def __init__(self):
         self.client = actionlib.SimpleActionClient('/pr2_pick_place/pp', pickplaceAction)
-        #self.client.wait_for_server()
+        self.client.wait_for_server()
         self.goal_id = 0
 
     def pick(self, obj):
         goal = pickplaceGoal()
 
         self.goal_id += 1
-        goal.id = self.goal_id
+        goal.id = "asdf"
 
         goal.operation = goal.PICK
 
-        goal.keep_orientation = True
+        goal.keep_orientation = False
         print "asdf \n\n"
         print obj
         goal.bb = obj['bbox']
@@ -42,6 +44,7 @@ class PickAndPlace:
         goal.pose.header.frame_id = obj['frame_id']
         goal.pose.header.stamp = rospy.Time.now()
         goal.pose.pose = obj['pose']
+        goal.pose.pose.position.z -= obj['bbox'].dimensions[2]/2
 
         rospy.loginfo('sending goal')
         self.client.send_goal(goal)
@@ -64,25 +67,55 @@ class PickAndPlace:
 
         goal.pose2 = pose
         goal.pose2.header.stamp = rospy.Time.now()
-        goal.pose2.pose.position.z = 0.74 + obj.bbox.dimensions[2]/2
+        goal.pose2.pose.position.z = 0.74 + 0.06# + obj.bbox.dimensions[2]/2
 
 
 def obj_cb(objects_data):
+    global objects
     """
 
         :type objects_data: InstancesArray
         :return:
     """
+    objects = {}
     for obj in objects_data.instances:
         if isinstance(obj, ObjInstance):
             objects[obj.object_id] = {'pose': obj.pose, 'bbox': obj.bbox, 'frame_id': objects_data.header.frame_id}
+    
 
+def point_cb(point_data):
+    global objects
+    global object_key
+    global timestamp
+    #print len(objects)
+    min = [None, 1]
+    
+    for key,obj in objects.iteritems():
+        
+        diff = math.sqrt(math.pow((obj['pose'].position.x - point_data.pose.position.x),2) + 
+                    math.pow((obj['pose'].position.x - point_data.pose.position.x),2) + 
+                    math.pow((obj['pose'].position.x - point_data.pose.position.x),2)) 
+	
+        if diff < min[1]:
+            min[1] = diff
+            min[0] = key
+    print min[1]    
+    if min[1] < 0.1:
+        print min[0]
+        if timestamp is None:
+            timestamp = rospy.Time.now()
+        elif (rospy.Time.now() - timestamp) > rospy.Duration(2):
+	    object_key =  min[0]
+    else:
+        timestamp = None
+    pass
 
 def main(args):
 
     rospy.init_node('art_pick_and_place')
-
-    obj_sub = rospy.Subscriber('/art_object_detector/object', InstancesArray, obj_cb)
+    point_sub = rospy.Subscriber('/pointing_point', PoseStamped, point_cb)
+    global object_key
+    obj_sub = rospy.Subscriber('/art_object_detector/object_filtered', InstancesArray, obj_cb)
     state = NOTHING
     pick_and_place = PickAndPlace()
     holding_obj_id = None
@@ -90,21 +123,23 @@ def main(args):
     try:
 
         while not rospy.is_shutdown():
-            print state
+            #print state
             if state == NOTHING:
-                obj_id = rospy.wait_for_message("/object_to_pick", String)
-                print obj_id.data
-                print objects
-                if obj_id.data in objects.keys():
-                    holding_obj_id = obj_id.data
-                    pick_and_place.pick(objects[holding_obj_id])
-                    state = PICKED
+                #obj_id = rospy.wait_for_message("/object_to_pick", String)
+                if object_key is not None:
+		
+                    
+                    if object_key in objects.keys():
+                        holding_obj_id = object_key
+                        pick_and_place.pick(objects[holding_obj_id])
+                        state = PICKED
+                    object_key = None
             elif state == PICKED:
                 if holding_obj_id is not None:
                     place_to_put = rospy.wait_for_message("/place_to_put", PoseStamped)
                     print place_to_put
 
-                    pick_and_place.place(place_to_put, objects[obj_id])
+                    pick_and_place.place(place_to_put, objects[holding_obj_id])
 
                 state = NOTHING
     except KeyboardInterrupt:
