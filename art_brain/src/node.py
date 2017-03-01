d3e66c5ff4cda462ff253984bf25cd2b07bd2501
ZdenekM <imaterna@fit.vutbr.cz>
2017-03-01 12:18:49 +0100

    art_brain: sleep not needed anymore

--- art_brain/src/node.py
+++ art_brain/src/node.py
@@ -877,7 +877,6 @@ class ArtBrain(object):
     def learning_request_cb(self,
                             goal):  # type: LearningRequestGoal
         rospy.loginfo("Learning_request goal: " + str(goal.request))
-	rospy.sleep(2)
         instruction = self.state_manager.state.program_current_item  # type: ProgramItem
         result = LearningRequestResult()
         if goal.request == LearningRequestGoal.GET_READY:
