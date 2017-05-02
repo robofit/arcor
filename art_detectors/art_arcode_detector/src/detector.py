8c29599f7cc86d0cd4f1aed213df33268e15b0a1
zdenekm <imaterna@fit.vutbr.cz>
2017-05-02 15:56:22 +0200

    syntax

--- art_detectors/art_arcode_detector/src/detector.py
+++ art_detectors/art_arcode_detector/src/detector.py
@@ -46,7 +46,7 @@ class ArCodeDetector:
                 # TODO AR code ID / object type assignment should be done somewhere...
                 # type "profile_20_60" is just for example (used in art_db/test_db.py)
 
-                if aid = 21:
+                if aid == 21:
                     object_type = self.art.get_object_type("profile_20_60_longer")
                 else:
                     object_type = self.art.get_object_type("profile_20_60")
