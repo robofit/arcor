5decee12a8232ba1731c1d5a6bc74a61048b6284
ZdenekM <imaterna@fit.vutbr.cz>
2017-07-21 15:26:55 +0200

    simple trolley init script

--- art_db/scripts/simple_trolley.py
+++ art_db/scripts/simple_trolley.py
@@ -20,7 +20,8 @@ def store_program(prog):
     except rospy.ServiceException as e:
         print "Service call failed: " + str(e)
         return
-    
+
+
 def store_object_type(ot):
 
     rospy.wait_for_service('/art/db/object_type/store')
@@ -33,6 +34,7 @@ def store_object_type(ot):
         print "Service call failed: " + str(e)
         return
 
+
 def feeder_item(it_id, on_success=None, on_failure=0, obj_type=""):
 
     p = item(it_id, ProgramItem.PICK_FROM_FEEDER, on_success, on_failure)
@@ -41,6 +43,7 @@ def feeder_item(it_id, on_success=None, on_failure=0, obj_type=""):
     p.pose.append(ps)
     p.object.append(obj_type)
     return p
+
 
 def place_item(it_id, ref_id, on_success=None, on_failure=0):
 
@@ -51,6 +54,7 @@ def place_item(it_id, ref_id, on_success=None, on_failure=0):
     p.object.append("")
     return p
 
+
 def grid_item(it_id, ref_id, on_success=None, on_failure=0):
 
     p = item(it_id, ProgramItem.PLACE_TO_GRID, on_success, on_failure, ref_id=ref_id)
@@ -60,10 +64,11 @@ def grid_item(it_id, ref_id, on_success=None, on_failure=0):
     p.polygon.append(pp)
     return p
 
+
 def drill_item(it_id, ref_id, on_success=None, on_failure=0):
 
     p = item(it_id, ProgramItem.DRILL_POINTS, on_success, on_failure, ref_id=ref_id)
-    ps = PoseStamped() # pose is relative to the selected object
+    ps = PoseStamped()  # pose is relative to the selected object
     p.pose.append(ps)
     p.pose.append(ps)
     p.object.append("")
@@ -71,6 +76,7 @@ def drill_item(it_id, ref_id, on_success=None, on_failure=0):
     pp.header.frame_id = "marker"
     p.polygon.append(pp)
     return p
+
 
 def wait_item(it_id, on_success=None, on_failure=0):
 
@@ -80,7 +86,8 @@ def wait_item(it_id, on_success=None, on_failure=0):
     p.polygon.append(pp)
     return p
 
-def item(it_id, it_type, on_success, on_failure=0, ref_id=[]):
+
+def item(it_id, it_type, on_success=None, on_failure=0, ref_id=[]):
 
     if on_success is None:
         on_success = it_id + 1
@@ -94,13 +101,15 @@ def item(it_id, it_type, on_success, on_failure=0, ref_id=[]):
 
     return p
 
+
 def obj_type(type_name, bbx, bby, bbz):
-    
+
     ot = ObjectType()
     ot.name = type_name
     ot.bbox.type = SolidPrimitive.BOX
     ot.bbox.dimensions = [bbx, bby, bbz]
     return ot
+
 
 def main(args):
 
@@ -109,15 +118,15 @@ def main(args):
     # -------------------------------------------------------------------------------------------
     # Simplified trolley assembly: object types
     # -------------------------------------------------------------------------------------------
-    
-    store_object_type(obj_type("p40x40x200"), 0.04, 0.04, 0.2)
-    store_object_type(obj_type("p40x40x400"), 0.04, 0.04, 0.4)
-    store_object_type(obj_type("p40x40x500"), 0.04, 0.04, 0.5)
-    
+
+    store_object_type(obj_type("p40x40x200", 0.04, 0.04, 0.2))
+    store_object_type(obj_type("p40x40x400", 0.04, 0.04, 0.4))
+    store_object_type(obj_type("p40x40x500", 0.04, 0.04, 0.5))
+
     # -------------------------------------------------------------------------------------------
     # Simplified trolley assembly: program
     # -------------------------------------------------------------------------------------------
-    
+
     prog = Program()
     prog.header.id = 20
     prog.header.name = "Simple Trolley Assembly"
@@ -135,7 +144,7 @@ def main(args):
     # each side consists of four profiles (of two types)
     pb.items.append(feeder_item(3, obj_type="p40x40x400"))
     pb.items.append(grid_item(4, on_success=3, on_failure=5, ref_id=[3]))
-    
+
     pb.items.append(feeder_item(5, obj_type="p40x40x200"))
     pb.items.append(grid_item(6, on_success=5, on_failure=11, ref_id=[5]))
 
@@ -143,7 +152,7 @@ def main(args):
     pb.items.append(drill_item(11, on_success=11, on_failure=12, ref_id=[4]))
     pb.items.append(drill_item(12, on_success=12, on_failure=13, ref_id=[6]))
 
-    pb.items.append(item(13, ProgramItem.GET_READY), on_success=0)
+    pb.items.append(item(13, ProgramItem.GET_READY, on_success=0))
 
     # --- right side of the trolley ------------------------------------------------------
     pb = deepcopy(pb)
@@ -160,13 +169,13 @@ def main(args):
     pb.on_success = 2
     pb.on_failure = 0
     prog.blocks.append(pb)
-    
+
     pb.items.append(item(2, ProgramItem.WAIT_UNTIL_USER_FINISHES))
 
-    pb.items.append(feeder_item(1), obj_type="p40x40x200")
+    pb.items.append(feeder_item(1, obj_type="p40x40x200"))
     pb.items.append(grid_item(2, on_success=1, on_failure=3, ref_id=[1]))
 
-    pb.items.append(item(3, ProgramItem.GET_READY), on_success=0)
+    pb.items.append(item(3, ProgramItem.GET_READY, on_success=0))
 
     store_program(prog)
 
