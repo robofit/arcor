5fc992c8938353661907bedc9e73340c248b1939
dev <dev@artable>
2017-08-03 09:26:44 +0200

    art_calibration: icp update

--- art_calibration/src/calibration.py
+++ art_calibration/src/calibration.py
@@ -10,6 +10,10 @@ from std_msgs.msg import Bool
 from art_msgs.srv import RecalibrateCell, RecalibrateCellRequest, RecalibrateCellResponse
 from pcl.registration import icp, icp_nl, gicp
 import tf
+from std_msgs.msg import Header
+
+from tf2_sensor_msgs.tf2_sensor_msgs import do_transform_cloud
+import sensor_msgs.point_cloud2 as pc2
 
 
 class ArtCalibration(object):
@@ -22,12 +26,12 @@ class ArtCalibration(object):
 
         for cell in cell_names:
             self.cells.append(ArtCellCalibration(cell, '/art/' + cell + '/ar_pose_marker',
-                                                 '/' + cell + '_kinect2_link',
-                                                 '/marker_detected', '/' + cell_names[0] + '_kinect2_link',
+                                                 '/marker_detected', '/' + cell + '_kinect2_link',
+                                                 '/' + cell_names[0] + '_kinect2_link',
                                                  '/art/' + cell + '/kinect2/qhd/points', self.listener))
 
         self.cells.append(ArtRobotCalibration('pr2', '/pr2/ar_pose_marker',
-                                              '/odom_combined', '/marker_detected',
+                                              '/marker_detected', '/odom_combined',
                                               '/' + cell_names[0] + '_kinect2_link',
                                               '/pr2/points', self.listener))
 
@@ -64,8 +68,8 @@ class ArtCalibration(object):
             if cell.calibrated:
                 tr = cell.get_transform()
                 self.broadcaster.sendTransform(tr.translation, tr.rotation,
-                                               time, cell.world_frame,
-                                               cell.cell_frame)
+                                               time, cell.cell_frame,
+                                               cell.world_frame)
 
             else:
                 calibrated = False
@@ -76,19 +80,46 @@ class ArtCalibration(object):
             self.calibrated_pub.publish(self.calibrated)
 
     def calculate_icp(self):
-        return
         print "calculate"
         main_cell = self.cells[0]  # type: ArtRobotCalibration
         for c in self.cells:  # type: ArtRobotCalibration
-            if c is main_cell:
-                continue
-            if c.last_pc is None or not c.calibrated:
-                return
 
-            converged, transf, estimate, fitness = icp(main_cell.last_pc, transformed_cloud, max_iter=25)
+            if c.last_pc_transformed is None or not c.calibrated:
+                return
+            if c is main_cell:
+                h = Header()
+                h.stamp = rospy.Time.now()
+                h.frame_id = c.world_frame
+
+                pcloud = pc2.create_cloud_xyz32(h, c.last_pc_transformed.to_list())
+                c.pc_pub.publish(pcloud)
+                continue
+            converged, transf, estimate, fitness = icp(main_cell.last_pc_transformed, c.last_pc_transformed, max_iter=25)
 
             print converged
             print fitness
+            h = Header()
+            h.stamp = rospy.Time.now()
+            h.frame_id = c.world_frame
+            print transf
+            tt = transformations.inverse_matrix(transf)
+            ttt = transformations.concatenate_matrices(transformations.translation_matrix(c.get_transform().translation),
+                                                       transformations.quaternion_matrix(c.get_transform().rotation))
+            translation2 = transformations.translation_from_matrix(
+
+                transformations.translation_matrix(transformations.translation_from_matrix(tt)) *
+                transformations.translation_matrix(c.get_transform().translation))
+            rotation2 = transformations.quaternion_from_matrix(
+
+                transformations.quaternion_matrix(transformations.quaternion_from_matrix(tt)) *
+                transformations.quaternion_matrix(c.get_transform().rotation))
+
+            #translation = transformations.translation_from_matrix(tttt)
+            #rotation = transformations.quaternion_from_matrix(tttt)
+            pcloud = pc2.create_cloud_xyz32(h, c.last_pc_transformed.to_list())
+            transformed_cloud = c.transform_pcloud(c.last_pc, translation2, rotation2,
+                                                   c.world_frame, c.world_frame)
+            c.pc_pub.publish(transformed_cloud)
 
 
 if __name__ == '__main__':
